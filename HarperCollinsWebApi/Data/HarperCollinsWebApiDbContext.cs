f069408e1ac217aa79c7c38ef21124b35290bd8f
panania <paanania@gmail.com>
2019-05-12 20:49:29 -0400

    First Commit

---
+++ HarperCollinsWebApi/Data/HarperCollinsWebApiDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HarperCollinsWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HarperCollinsWebApi.Data
+{
+    public class HarperCollinsWebApiDbContext : DbContext
+    {
+        public HarperCollinsWebApiDbContext(DbContextOptions<HarperCollinsWebApiDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<SaleOrder> SaleOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Title>().ToTable("Title");
+            modelBuilder.Entity<SaleOrder>().ToTable("SaleOrder");
+        }
+    }
+}
---
+++ HarperCollinsWebApi/Migrations/20190411153757_Azure.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HarperCollinsWebApi.Migrations
+{
+    public partial class Azure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CustomerNumber = table.Column<string>(nullable: true),
+                    CustomerName = table.Column<string>(nullable: true),
+                    AddressLine1 = table.Column<string>(nullable: true),
+                    AddressLine2 = table.Column<string>(nullable: true),
+                    AddressLine3 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Title",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ISBN = table.Column<string>(nullable: true),
+                    PublicationTitle = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    PublicationFormat = table.Column<string>(nullable: true),
+                    ListPrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Title", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SaleOrder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CustomerId = table.Column<int>(nullable: false),
+                    TitleId = table.Column<int>(nullable: false),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SaleOrder", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SaleOrder_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SaleOrder_Title_TitleId",
+                        column: x => x.TitleId,
+                        principalTable: "Title",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleOrder_CustomerId",
+                table: "SaleOrder",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleOrder_TitleId",
+                table: "SaleOrder",
+                column: "TitleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SaleOrder");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Title");
+        }
+    }
+}
